cf6a8d2d26b1a0d563aaf283d29ed00f44faefe5
Rafael de Luca <deluca1712@gmail.com>
2022-05-11 10:55:19 -0300

    Adicionando o MovieStars, MovieScore e MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,36 @@
+import MovieCard from "components/MovieCard";
+//import MovieStars from "components/MovieStars";
+//import MovieScore from "components/MovieScore";
 import Pagination from "components/Pagination";
 
-function Listing () {
+function Listing() {
     return (
-        <Pagination></Pagination>
+        <>
+            <Pagination></Pagination>
+            <div className="row">
+                        
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-4"><MovieCard></MovieCard></div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-4"><MovieCard></MovieCard></div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-4"><MovieCard></MovieCard></div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-4"><MovieCard></MovieCard></div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-4"><MovieCard></MovieCard></div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-4"><MovieCard></MovieCard></div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-4"><MovieCard></MovieCard></div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-4"><MovieCard></MovieCard></div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-4"><MovieCard></MovieCard></div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-4"><MovieCard></MovieCard></div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-4"><MovieCard></MovieCard></div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-4"><MovieCard></MovieCard></div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-4"><MovieCard></MovieCard></div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-4"><MovieCard></MovieCard></div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-4"><MovieCard></MovieCard></div>
+
+             
+            </div>
+
+
+
+        </>
     );
 }
 
